f5502138fd3b787e3ae55a5d188caddb75723ed4
Amir Zakariah <156103488+amirzach@users.noreply.github.com>
2024-12-13 18:42:34 +0800

    Corrected App.js

--- App.js
+++ App.js
@@ -1,34 +1,19 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
-import GradeSelection from "./components/GradeSelection";
-import Questionnaire from "./components/Questionnaire";
-import Recommendation from "./components/Recommendation";
-import "./styles.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginScreen from './components/LoginScreen';
+import SignupScreen from './components/SignupScreen';
+import ResetPasswordScreen from './components/ResetPasswordScreen';
+import Register from './components/Register';
 
-function App() {
-  const [grades, setGrades] = useState({});
-  const [responses, setResponses] = useState({});
-
+const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route
-            path="/"
-            element={<GradeSelection onNext={(submittedGrades) => setGrades(submittedGrades)} />}
-          />
-          <Route
-            path="/questionnaire"
-            element={<Questionnaire grades={grades} onSubmit={(submittedResponses) => setResponses(submittedResponses)} />}
-          />
-          <Route
-            path="/recommendation"
-            element={<Recommendation responses={responses} />}
-          />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<SignupScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/reset-password" element={<ResetPasswordScreen />} />
+        <Route path="/register" element={<Register />} />        
+      </Routes>
   );
-}
+};
 
 export default App;
